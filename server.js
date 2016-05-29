bcb3fd4279bce132c54c844c5d715b11d3879e53
Jacob Heater <jacobheater@gmail.com>
2016-05-29 01:27:07 -0400

    Minor changes
    Adding README for new example
    Adding small updates to autoSaveTextbox markup
    Small mods to server.js

--- server.js
+++ server.js
@@ -5,18 +5,18 @@
     var cmd = require('child_process').exec;
 
     var paths = ['/src', '/examples'];
-    var defaults = ['/', '/examples'];
+    var defaults = ['/examples', '/countries'];
     var port = 1024;
 
     //Make these directories static so that their files can be served.
     paths.map(p => app.use(p, express.static(__dirname.concat(p))));
     //Redirect the user to the appropriate demo if any of the given default directories are used.
-    defaults.map(d => app.get(d, (request, response) => response.redirect(302, "/examples/countries")));
+    defaults.map(d => app.get(d, (request, response) => response.redirect(302, `/examples${d}`)));
 
     app.listen(port, function() {
         console.log(`Listening on port ${port}.`);
         //Open chrome in a new window pointing to the address of our application.
-        cmd(`start cmd /c start chrome --app="http://localhost:${port}"`, (error, stdout, stderr) => {
+        cmd(`start cmd /c start chrome http://localhost:1024/countries"`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 return;
