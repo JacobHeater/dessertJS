a959753d3c77e79f67dd4f2053aa9a5f9bca93aa
Jacob Heater <jacobheater@Jacobs-MBP.fios-router.home>
2016-09-08 08:38:25 -0400

    Refactoring view.

--- src/dessert.view.js
+++ src/dessert.view.js
@@ -7,10 +7,25 @@
         ],
         function(events) {
             //A View is a collection of Controls
-            var View = function(name, controller, $view) {
+            /**
+             * A view in dessertJS is a self-explanatory component, it represents
+             * the visual element of your application. That being said, Views are
+             * comprised of Controls in the View. The Controls are elements in the View
+             * marked with the Control attribute. They are compiled at the time the application
+             * is loaded, and are added to a hash table by their name. So, essentially,
+             * a View is a collection of Controls on the page, that can be interacted with.
+             * 
+             * @param {String} name The name of the view.
+             * @param {Object} controller The controller that the view is associated with.
+             * @param {Object} $view The jQuery object that represents the view on the page.
+             */
+            function View(name, controller, $view) {
                 this.name = name || "";
                 this.controller = controller;
                 this.$view = $view;
+                /**
+                 * Destroys the view on the page.
+                 */
                 this.destroy = function() {
                     this.$view.remove();
                 };
@@ -28,15 +43,33 @@
                     return this;
                 };
                 this.controls = {
+                    /**
+                     * Adds a control instance to the view controls hash table.
+                     * 
+                     * @param {Object} control The control instance to add.
+                     * @returns {Object} The current instance of the controls object.
+                     */
                     add: function(control) {
                         if (control && control.dsrt) {
                             this[control.dsrt.name] = control;
                         }
                         return this;
                     },
+                    /**
+                     * Gets a control in the view hash table by name.
+                     * 
+                     * @param {String} name The name of the control to look up.
+                     * @returns {Object} The control in the hash table.
+                     */
                     get: function(name) {
                         return this[name];
                     },
+                    /**
+                     * Removes a control by name from the hash table.
+                     * 
+                     * @param {String} name The name of the control to remove.
+                     * @returns {Object} The current instance of the controls object.
+                     */
                     remove: function(name) {
                         delete this[name];
                         return this;
