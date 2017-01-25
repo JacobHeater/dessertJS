4d2793b8fa3e7a6cbe2f5bf9c3dba9cf69dfad22
Jacob Heater <jacobheater@Jacobs-MacBook-Pro.local>
2017-01-25 03:13:55 -0500

    Minifying distros.

--- gulp/tasks/build.debug.js
+++ gulp/tasks/build.debug.js
@@ -1,8 +1,10 @@
 (() => {
     var gulp = require('gulp');
+    var uglify = require("gulp-uglify");
     
     gulp.task('build-debug', ['clean'], function() {
         gulp.src('src/**/*.js')
+        .pipe(uglify())
         .pipe(gulp.dest("./bin/dessertJS"))
         .pipe(gulp.dest("./examples/comprehensive/lib/dessertJS"));
     });
